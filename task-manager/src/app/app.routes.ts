27b0874c0e5db06e92829943d746e26f723905fb
clement kieu <c.kieu@lyon.ort.asso.fr>
2025-11-26 17:00:58 +0100

    fin de journée

--- task-manager/src/app/app.routes.ts
+++ task-manager/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { TaskList } from './task-list/task-list';
 import { HomeComponent } from './home-component/home-component';
 import { TaskDetail } from './task-detail/task-detail';
+import {Statistics} from './statistics/statistics';
 
 export const routes: Routes = [
     { path: 'tasks', component: TaskList },
     { path: 'Accueil', component: HomeComponent },
     { path: 'task/:index', component: TaskDetail },
+    { path: 'statistic', component: Statistics},
 ];
--- task-manager/src/app/task.ts
+++ task-manager/src/app/task.ts
@@ -6,7 +6,17 @@ import { Injectable } from '@angular/core';
 })
 export class TaskService {
   //tasks = [{ nom: 'un', etat_terminer: true }, { nom: 'deux', etat_terminer: false }, { nom: 'trois', etat_terminer: true }];
-  tasks: Task[] = [{ name: 'un', completed: true }, { name: 'deux', completed: false }, { name: 'trois', completed: true }];
+
+  tasks: Task[] = [];
+
+  // fonction initial de angular 
+  constructor() {
+    let localstorage = localStorage.getItem('tasks');
+    if (localstorage != null)
+    {
+      this.tasks = JSON.parse(localstorage);
+    }
+  }
 
   getTasks() {
     return this.tasks;
@@ -19,40 +29,47 @@ export class TaskService {
   addTask(task: Task) {
     /**inserer newTask dans le tableau tasks */
     this.tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 
   deleteTask(i: number) {
     this.tasks.splice(i, 1);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+
   }
 
   switchEtat(i: number) {
     let obj = this.tasks[i];
     if (obj.completed == true) {
       obj.completed = false;
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+
     }
     else {
       obj.completed = true;
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+
     }
   }
 
   // fonction pour modifier un name 
-  modifName(id : number, name : string) {
+  modifName(id: number, name: string) {
     // recupère le nom et le remplacer par un autre
     this.tasks[id].name = name;
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 
-  modifCompleted(id : number, ) {
+  modifCompleted(id: number,) {
     // recupère le nom et le remplacer par un autre
     this.tasks[id].completed = !this.tasks[id].completed;
-  }
-
-
-  modifier() {
-
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
 
 export interface Task {
   name: string;
   completed: boolean;
+  /*type_de_tache: string;
+  criticite: number;*/
+
 }
